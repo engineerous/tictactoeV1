85b8be5eaf2b4a2fba99a64f3c4451096abb7f00
JP <jonpaulvd@hotmail.com>
2022-04-28 15:12:22 -0700

    Completed Pt 1

--- Level-2-Intermediate/src/main.js
+++ Level-2-Intermediate/src/main.js
@@ -2,35 +2,72 @@
 // and returns an array of only the odd numbers
 // ex: returnOdds([1,2,3,4,5,6,7]); -> [1,3,5,7]
 function returnOdds(array) {
-  // CODE HERE
-
+  //iterate through each element of array
+  // if modolo % 2  != 0, add to return array
+  //return the filtered array
+ let result = array.filter(element => element %2 > 0)
+return result; 
 }
 
+//console.log(returnOdds([1,2,3,4,5,6,7]))
 
 // this function accepts an array of numbers
 // and returns an array of only the even numbers
 // ex: returnEvent([1,2,3,4,5,6,7]); -> [2,4,6]
 function returnEvens(array) {
-  // CODE HERE
-
+  let result = array.filter(element => element %2 === 0)
+  return result;   
 }
-
+//console.log(returnEvens([1,2,3,4,5,6,7]))
 
 // returns only the max element from the inputted array of numbers
 // ex: findMax([1,25,6,3]); -> 25
 function findMax(array) {
-  // CODE HERE
+ let result = array.reduce((previousValue, currentValue, currentIndex) => { 
 
+  if (currentValue > previousValue)
+  previousValue = currentValue;
+  return previousValue;
+ }
+ )
+ return result;  
 }
 
+//console.log(findMax([1,25,6,3]))
 /**
  * remove leading and trailing whitespace or specified characters from string
  * trim(' hello '); -> 'hello'
  */
-function trim(string) {
-  // CODE HERE
-}
-
+  function trim(string) {
+    //convert string to array
+    let arry =  string.split('');
+    let result = []; 
+    console.log(arry)
+    //iterate through array up to first letter
+    let element = arry[0]
+    let counter = 0; 
+    while (element === ' ')
+    {
+    counter++;
+    element = arry[counter];
+    }
+    console.log(counter)
+    
+    let counterend = arry.length -1; 
+    element = arry[counterend]
+    while (element === ' ')
+    {
+    
+        counterend--;
+        element = arry[counterend]
+    }
+    console.log(counterend)
+    return arry.slice(counter, counterend +1).join("") 
+    // if element is white space, ignore
+    //otherwise add to result array
+    //return result array and concat back to string...
+      }
+//console.log(trim(" hello  "));
 // under the hood, a JavaScript array is a specific type of object in which values are paired with sequentially numbered keys.
 // the "Array" object also contains a number of methods that give arrays their functionality.
 // the below function should return an empty object that has the behavior and functionality of an array. this object should have the following methods:
@@ -40,6 +77,70 @@ function trim(string) {
   // shift() removes a value from the beginning and returns it
 // the goal of this problem is to reverse engineer what array methods are actually doing and create an object that has those methods
 function createArray() {
-  // CODE HERE
+  this.value = {};  
 
 }
+
+createArray.prototype.length= function () { 
+   let counter = 0; 
+   let element = this.value[counter];
+   while (element !== undefined)
+   {
+       counter++;
+       element = this.value[counter]
+   }
+  console.log(counter)
+  return counter; 
+  };
+
+  createArray.prototype.push= function (val) { 
+    this.value[this.length()] = val;
+     };
+    
+     createArray.prototype.pop= function () { 
+     let result = this.value[this.length() -1];
+      delete this.value[this.length() -1 ];
+      return result;
+       };
+
+       createArray.prototype.shift= function () { 
+          let counter =0; 
+          let result = this.value[counter];
+          let element = this.value[counter]; 
+          while (element !== undefined && this.value[counter +1] !== undefined ){
+              this.value[counter] = this.value[counter+1];
+              counter++; 
+              element = this.value[counter]
+              console.log(element)
+          }
+          delete this.value[this.length() -1]
+          return result; 
+            };
+
+            createArray.prototype.shift= function () { 
+              let counter =0; 
+              let result = this.value[counter];
+              let element = this.value[counter]; 
+              while (element !== undefined && this.value[counter +1] !== undefined ){
+                  this.value[counter] = this.value[counter+1];
+                  counter++; 
+                  element = this.value[counter]
+                 // console.log(element)
+              }
+              delete this.value[this.length() -1]
+              return result; 
+                };
+  
+                createArray.prototype.unshift= function(val) { 
+                 let counter = this.length(); 
+                 while (counter > 0 )
+                 {
+                     this.value[counter ] = this.value[counter-1]
+                   counter--
+                 }
+                 this.value[0] = val;
+                 return this.length()
+                    };
+let myArry = new createArray(); 
+console.log(myArry.push("jason"))
+console.log(myArry)
--- Level-3-Advanced/src/main.js
+++ Level-3-Advanced/src/main.js
@@ -5,8 +5,22 @@
 // forEach(['a','b','c'], callback); → prints a,0,['a','b','c'] b,1,['a','b','c'] c,2,['a','b','c']
 // For each element in the array, the callback we passed is called. The callback can be customized, but in the above example, the callback prints out the element, index, and entire array.
 function forEach(array, callback) {
-  // CODE HERE
+  let result = []; 
+for (let i =0; i < array.length; i++)
+{
+result.push( callback(array[i], i, array));
+
 }
+  
+return result; 
+}
+  
+  
+  const callback = function(element, index, array) {
+       console.log(element +"," +index + ",[" + array + "]");
+  }
+  
+  forEach(['a','b','c'], callback);
 
 // Creates an array of values by running each element in collection through callback
 // Should we explain that map returns?
@@ -16,8 +30,15 @@ function forEach(array, callback) {
 // }); -> [3,6,9]
 // BONUS: use the forEach method you use to create map
 function map(array, callback) {
-  // CODE HERE
-}
+  let result = forEach(array, callback)
+  return result
+  }
+  
+  console.log(map([1,2,3], function(element, index, array) {
+      return element * 3;
+     }));
+
+
 
 // Iterates over elements of collection, returning an Array of all elements callback returns truthy for.
 // filter([1,2,3,4], function(element, index, collection) {
@@ -27,8 +48,20 @@ function map(array, callback) {
 //  return element % 2 !== 0;
 // }); → [1,3]
 function filter(collection, callback) {
-  // CODE HERE
+  let result =[];
+for (let i = 0; i < collection.length; i++)
+{
+ if (callback(collection[i])=== true)
+ result.push(collection[i]);
+
 }
+return result; 
+
+}
+
+console.log(filter([1,2,3,4], function(element, index, collection) {
+   return element % 2 === 0;
+   }));
 
 // Removes all elements from array that callback returns truthy for and returning a collection of elements that did not pass the truthy test.
 // The returned collection should be the same type that was passed in, either an Array or Object.
@@ -40,15 +73,52 @@ function filter(collection, callback) {
 // }); → {b:2, d:4}
 // Challenge: use filter
 function reject(collection, callback) {
-  // CODE HERE
-}
 
+  if (Array.isArray(collection) == true)
+  {
+   let result = [];
+   for (let i = 0; i < collection.length; i++ )
+   {
+
+    if (callback(collection[i]) == false)
+    result.push(collection[i])
+   }
+ return result; 
+  }
+  
+  if (Object.prototype.toString.call(collection) === '[object Object]')
+  { let result = {}
+    for (let i in collection)
+    {
+      if (callback(collection[i])== false)
+      result[i] = collection[i]
+
+    }
+    return result; 
+
+
+
+  }
+  
+  }
+  
+  reject({a:1, b:2, c:3, d:4}, function(value, key, collection) {
+    return value % 2 !== 0;
+     });
+
+      reject([1,2,3,4], function(element, index, collection) {
+       return element % 2 === 0;
+       });
 // Creates an array without duplicate values from the inputted array.
 // The order of the array is preserved.
 // uniq([1,2,1]); → [1,2]
 function uniq(array) {
-  // CODE HERE
+return array.reduce((previousValue, currentValue, currentIndex, array) => { 
+if (previousValue.includes(currentValue) == false)
+previousValue.push(currentValue);
+return previousValue;
 
+}, [])
 }
 
 // Gets the index at which the first occurrence of value is found in array
@@ -57,17 +127,41 @@ function uniq(array) {
 // indexOf([11,22,33], 11); → 0
 // indexOf([11,22,33], 5); → -1
 function indexOf(array, value) {
-  // CODE HERE
+  let result = -1;
+for (let i = 0; i < array.length; i++)
+{
 
+  if (array[i] === value)
+  {result = i; 
+  return result; 
+  }
+}
+return result; 
 }
 
 
 // Returns a function that is restricted to invoking func once.
 // Repeat calls to the function return the value of the first call.
-function once(func) {
-  // CODE HERE
 
-}
+  function once(func) {
+    let counter = 0; 
+    let result = undefined;
+      function rFunc() {
+       
+        if (counter ===0)
+          {
+           result = func(); 
+            counter++;
+            
+          }
+          else
+          return result; 
+     
+        
+      }
+      return rFunc;
+    }
+
 
 // Reduces collection to a value which is the accumulated result of running each element in collection through iteratee, where each successive invocation is supplied the return value of the previous. If accumulator is not provided the first element of collection is used as the initial value.
 // If a start parameter is not provided, then set the start value as the zeroth index
@@ -78,7 +172,28 @@ function once(func) {
 //  return stored + current;
 // },1); → 4
 function reduce(array, callback, start) {
-  // CODE HERE
+let previousValue = 0;
+let currentValue = 0;
+let index = 0;
+  if (start == undefined)
+{
+previousValue = array[0];
+currentValue = array[1];
+index = 1; 
+}
+else
+{ previousValue = start;
+  currentValue = array[0];
+}
+
+
+for (let i = index; i < array.length; i++) {
+  currentValue =array[i]
+  previousValue = callback(previousValue, currentValue)
+}
+
+return previousValue; 
+
 }
 
 // Takes an array and a function as arguments.
@@ -92,18 +207,50 @@ function reduce(array, callback, start) {
 // });  -> false
 // BONUS: use reduce in your answer
 function every(array, func) {
-  // CODE HERE
-
+for (let i = 0; i < array.length; i++)
+{
+  if (func(array[i])== false)
+  return false; 
+}
+return true; 
 }
 
 // Flattens a nested array.
-// flatten([1, [2, 3, [4]]]); → [1, 2, 3, [4]]
-function flatten(array) {
-  // CODE HERE
+console.log(flatten([1, [2, 3, [4]]])); 
+
+function flatten(array,results =[]) {
+
+for (let i = 0; i < array.length; i++)
+{
+ if (array[i].length > 0)
+ {
+   for (let k = 0; k < array[i].length; k++)
+   results.push(array[i][k]);
+ }
+ else
+ {
+   console.log(array[i])
+   results.push(array[i])
+ }
+}
+return results; 
 }
 
 // Recursively flattens a nested array.
 // flattenDeep([1, [2, 3, [4]]]); → [1, 2, 3, 4]
-function flattenDeep(array) {
-  // CODE HERE
+function flattenDeep(array, results = []) {
+
+  for (let i = 0; i < array.length; i++)
+  {
+  if (array[i].length > 0)
+  {
+    results = flattenDeep(array[i], results)
+  }
+  else
+  {
+    console.log(array[i])
+    results.push(array[i])
+  }
+}
+ return results; 
 }
